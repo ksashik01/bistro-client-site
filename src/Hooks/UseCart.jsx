6135ad14d5d123486ef405abd1010b6c6ec5b4b6
ksashik01 <kscomplainsheet@gmail.com>
2023-06-04 02:35:26 +0600

    new File

--- src/Hooks/UseCart.jsx
+++ src/Hooks/UseCart.jsx
@@ -1,26 +1,36 @@
 
 import { useQuery } from '@tanstack/react-query'
-import { useContext } from 'react'
-import { AuthContext } from '../Provider/AuthProvider'
+
+import useAxiosSecure from './useAxiosSecure';
+import useAuth from './useAuth';
 
 // tanStack Query-----------
 
 const useCart = () =>{
-    const {user} = useContext(AuthContext);
-    const token = localStorage.getItem('access-token')
+    const {user} =useAuth();
+    // const token = localStorage.getItem('access-token');
+    const [axiosSecure]   = useAxiosSecure();
+
     const { refetch, data: cart = [] } = useQuery({
         queryKey: ['carts', user?.email],
+        // queryFn: async () =>{
+        //     const res = await fetch(`http://localhost:5000/carts?email=${user?.email}`,{
+        //         headers:{
+        //             authorization:`bearer ${token}`
+        //         }
+        //     })
+        //     console.log(res)
+        //     return res.json();
+        // },
         queryFn: async () =>{
-            const res = await fetch(`http://localhost:5000/carts?email=${user?.email}`,{
-                headers:{
-                    authorization:`bearer ${token}`
-                }
-            })
-            console.log(res)
-            return res.json();
+            const res = await axiosSecure(`/carts?email=${user?.email}`)
+            console.log('res from axios', res)
+
+        
+            return res.data;
         },
       })
 
       return [cart, refetch]
 }
-export default useCart
+export default useCart  
--- src/Pages/Dashboard/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard/Dashboard.jsx
@@ -1,65 +1,111 @@
 import { NavLink, Outlet } from "react-router-dom";
-import { FaShoppingCart, FaWallet, FaCalendarAlt,FaHome, FaUtensils,FaBook } from 'react-icons/fa';
+import {
+  FaShoppingCart,
+  FaWallet,
+  FaCalendarAlt,
+  FaHome,
+  FaUtensils,
+  FaBook,
+} from "react-icons/fa";
 import { BsMenuDown } from "react-icons/bs";
 import useCart from "../../../Hooks/UseCart";
-
-
 
 const Dashboard = () => {
   const [cart] = useCart();
   const isAdmin = true;
-    return (
-        <div className="drawer drawer-mobile">
-        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-        <div className="drawer-content flex flex-col items-center justify-center">
-          {/* <!-- Page content here --> */}
-          <Outlet></Outlet>
-          <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden"></label>
-        
-        </div> 
-        <div className="drawer-side bg-[#D1A054]">
-          <label htmlFor="my-drawer-2" className="drawer-overlay"></label> 
-          <ul className="menu p-4 w-80 ">
-            {/* <!-- Sidebar content here --> */}
-          {
-            isAdmin?
-            <>
-            <li><NavLink to="home"><FaHome></FaHome>Admin</NavLink></li>
-              
-                <li><NavLink to="dashboard/reservation"><FaUtensils></FaUtensils>Add Items</NavLink></li>
-                <li><NavLink to="/dashboard/reservation"><FaUtensils></FaUtensils>Manage Items</NavLink></li>
-                <li><NavLink to="/dashboard/history"><FaBook></FaBook>Manage Bookins</NavLink></li>
-                <li><NavLink to="/dashboard/allusers"><FaBook></FaBook>All Users</NavLink></li>
-                <p>Hello</p>
-            
-            </>
-            :
-            
-            <>
-            <li><NavLink to="home"><FaHome></FaHome>User Home</NavLink></li>
-                <li><NavLink to="dashboard/cart"><FaShoppingCart></FaShoppingCart>My Cart
-                <span className="badge badge-secondary">+{cart?.length || 0}</span>
-                
-                </NavLink></li>
-                <li><NavLink to="dashboard/history"><FaWallet></FaWallet>Payment History</NavLink></li>
-                <li><NavLink to="dashboard/reservation"><FaCalendarAlt></FaCalendarAlt>Reservation</NavLink></li>
-                <div className="divider"></div>
-                <li><NavLink><FaHome></FaHome>Home</NavLink></li>
-                <li><NavLink to="/menu"> <FaWallet></FaWallet> Our Menu</NavLink></li>
-        <li><NavLink to="/order/salad"><BsMenuDown></BsMenuDown>Our Order</NavLink></li>
-
-            </>
-          }
-
-
-
-                
-          
-          </ul>
-       
-        </div>
+  return (
+    <div className="drawer drawer-mobile">
+      <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content flex flex-col items-center justify-center">
+        {/* <!-- Page content here --> */}
+        <Outlet></Outlet>
+        <label
+          htmlFor="my-drawer-2"
+          className="btn btn-primary drawer-button lg:hidden"
+        ></label>
       </div>
-    );
+      <div className="drawer-side bg-[#D1A054]">
+        <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+        <ul className="menu p-4 w-80 ">
+          {/* <!-- Sidebar content here --> */}
+          {isAdmin ? (
+            <>
+              <li>
+                <NavLink to="home">
+                  <FaHome></FaHome>Admin
+                </NavLink>
+              </li>
+
+              <li>
+                <NavLink to="dashboard/reservation">
+                  <FaUtensils></FaUtensils>Add Items
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/reservation">
+                  <FaUtensils></FaUtensils>Manage Items
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/history">
+                  <FaBook></FaBook>Manage Bookins
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/allusers">
+                  <FaBook></FaBook>All Users
+                </NavLink>
+              </li>
+              <p>Hello</p>
+            </>
+          ) : (
+            <>
+              <li>
+                <NavLink to="home">
+                  <FaHome></FaHome>User Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="dashboard/cart">
+                  <FaShoppingCart></FaShoppingCart>My Cart
+                  <span className="badge badge-secondary">
+                    +{cart?.length || 0}
+                  </span>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="dashboard/history">
+                  <FaWallet></FaWallet>Payment History
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="dashboard/reservation">
+                  <FaCalendarAlt></FaCalendarAlt>Reservation
+                </NavLink>
+              </li>
+              <div className="divider"></div>
+              <li>
+                <NavLink>
+                  <FaHome></FaHome>Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/menu">
+                  {" "}
+                  <FaWallet></FaWallet> Our Menu
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/order/salad">
+                  <BsMenuDown></BsMenuDown>Our Order
+                </NavLink>
+              </li>
+            </>
+          )}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
